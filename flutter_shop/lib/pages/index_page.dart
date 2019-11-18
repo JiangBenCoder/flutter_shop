cee7b498f3de86d9e81da2637a696020dd552095
jiangben <2284634727@qq.com>
2019-11-18 11:46:57 +0800

    商城项目初始化

---
+++ flutter_shop/lib/pages/index_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  final List<BottomNavigationBarItem> items = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search),
+        title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart),
+        title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.person),
+        title: Text('会员中心')
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
